c426038e1ddfa705c86365c8c387b0cf4dc0990e
webDevSaurav <webdevelopersaurav@gmail.com>
2020-07-04 16:07:12 +0530

    Updates

---
+++ precache-manifest.5ab8075ae7b28acdee956785f416cc04.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d6ffe188f9e71ace64da5cbec1ba4e2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7e0082d6a771414e1a63",
+    "url": "/portfolio/static/css/main.69d858d0.chunk.css"
+  },
+  {
+    "revision": "5a7750be9dc0a78b8539",
+    "url": "/portfolio/static/js/2.77280385.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/portfolio/static/js/2.77280385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e0082d6a771414e1a63",
+    "url": "/portfolio/static/js/main.fe7b18fb.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "273fb0d24fd5d347d992b3060eadaa1d",
+    "url": "/portfolio/static/media/budget-app.273fb0d2.png"
+  },
+  {
+    "revision": "fceb5c64f8f49fe35b0256ac2260321c",
+    "url": "/portfolio/static/media/corona-tracker.fceb5c64.png"
+  },
+  {
+    "revision": "816e325fe952f0cb0d50edffce0ea262",
+    "url": "/portfolio/static/media/myPic.816e325f.png"
+  },
+  {
+    "revision": "6d3067df8fb1a83d22ec4180ea41905c",
+    "url": "/portfolio/static/media/todo-list.6d3067df.png"
+  }
+]);
